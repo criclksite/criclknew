39b7f363e45438b0245d3ccd138b310056054e4c
cyberdevlk <138901572+cyberdevlk@users.noreply.github.com>
2025-04-16 19:13:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Store stream URL securely on server side
-// sandbix const STREAM_URL = 'https://ams.sandbrix.live/LiveApp/streams/vC19gZapiqRVC2ry35467275934172.m3u8';
+// sandbix
+const STREAM_URL = 'https://ams.sandbrix.live/LiveApp/streams/vC19gZapiqRVC2ry35467275934172.m3u8';
 // Tsport
 // const STREAM_URL = 'https://live-test.tsports.com/live-02/master_1080.m3u8?hdntl=Expires=1744293989~_GO=Generated~URLPrefix=aHR0cHM6Ly9saXZlLXRlc3QudHNwb3J0cy5jb20v~Signature=AVTZpUuvMAO0jMjVeCqYzxZGHNJAbHTlTRUDnSB_tOogMNAb2O8PbntNmK9b6Bd7nzjkrPaWLsrB8rPMt6J0MsbpBlAM';
 // smartcric.ch 
-const STREAM_URL = 'https://003.fclplayer.net/live/csstream1/chunklist.m3u8?vidictid=201886909031&id=1001&pk=b1f3e63c5bb79b8bef6fe4b35c7d59b01ab29937fa20606825cebb7c2fdb3b43';
+//const STREAM_URL = 'https://003.fclplayer.net/live/csstream1/chunklist.m3u8?vidictid=201886909031&id=1001&pk=b1f3e63c5bb79b8bef6fe4b35c7d59b01ab29937fa20606825cebb7c2fdb3b43';
 
 //https://eplayhd.com/live/tsports/
 //  const STREAM_URL = 'https://live.tsports.com/?bitrate=1024000&channel=tsports_live_2_720&res=720'
